fa11dd1a2ca32d76e3c3626d4c121c5435cd7996
742942Ayan <pgdisha9520@gmail.com>
2025-07-06 18:31:36 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,1 +1,10 @@
-// TODO: Add code
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://api.adglow.in', // ✅ Replace with real backend URL
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default api;
