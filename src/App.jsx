af8b82e735162df02f81040e860fca67751bd3cd
742942Ayan <pgdisha9520@gmail.com>
2025-07-06 10:51:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,30 @@
-<Route path="/watch-ads" element={<WatchAds />} />
-<Route path="/withdraw" element={<Withdraw />} />
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
+import Dashboard from './pages/Dashboard';
+import WatchAds from './pages/WatchAds';
+import Withdraw from './pages/Withdraw';
+// Future: TeamTree, KycUpload, Leaderboard, Profile, etc.
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/watch-ads" element={<WatchAds />} />
+        <Route path="/withdraw" element={<Withdraw />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
