46d38912de10d85e060342820a96a6160fdb6f4c
742942Ayan <pgdisha9520@gmail.com>
2025-07-22 17:16:35 +0530

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,27 +1,25 @@
 // src/utils/axiosInstance.js
-import axios from 'axios';
+import axios from "axios";
 
-// Token ko localStorage se read karo
-const token = localStorage.getItem("adglow_token");
-
-// Axios instance create karo
 const axiosInstance = axios.create({
-  baseURL: 'https://adglow-backend.onrender.com/api',
+  baseURL: "https://adglow-backend.onrender.com/api",
   headers: {
-    'Content-Type': 'application/json',
-    Authorization: token ? `Bearer ${token}` : '', // token lagao agar mila to
+    "Content-Type": "application/json",
   },
 });
 
-// Har request se pehle token update karne ke liye interceptor
-axiosInstance.interceptors.request.use((config) => {
-  const newToken = localStorage.getItem("adglow_token");
-  if (newToken) {
-    config.headers.Authorization = `Bearer ${newToken}`;
+// Add token to every request
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("adglow_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 export default axiosInstance;
